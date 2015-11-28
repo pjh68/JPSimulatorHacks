c7e60d63b9576ddd83552596de28da2f17c76d0f
Peter Hempsall <phempsall@deloitte.co.uk>
2015-11-28 22:54:15 +0000

    Fix for framework (carthage)

--- Pods/JPSimulatorHackFramework/JPSimulatorHackFramework.h
+++ Pods/JPSimulatorHackFramework/JPSimulatorHackFramework.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char JPSimulatorHackFrameworkVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <JPSimulatorHackFramework/PublicHeader.h>
 
 #import "JPSimulatorHacks.h"
+#import "JPSimulatorHacksDB.h"
